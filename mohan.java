c2153cabde9047cdfc032185a1f8b4d500adb762
thirumalarao9182 <142675050+thirumalarao9182@users.noreply.github.com>
2023-08-30 14:36:31 +0530

    Add files via upload

---
+++ mohan.java
@@ -0,0 +1,39 @@
+class Defaultvalues
+{
+byte b;
+short s;
+int i;
+long l;
+float f;
+double d;
+char c;
+boolean b;
+string s;
+public static void main(string[]args)
+{
+Default values df=new Defaultvalues();
+System.out.print/n("Byte:"+df.b);
+System.out.print/n("Short:"+df.s);
+System.out.print/n("Int:"+df.i);
+System.out.print/n("Long:"+df.l);
+System.out.print/n("Float:"+df.f);
+System.out.print/n("Double:"+df.d);
+System.out.print/n("Char:"+df.c);
+System.out.print/n("Boolean:"+df.b);
+System.out.print/n("String:"+df.s);
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
